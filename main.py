79be6476075b0da4f800eeceafd14f81fb6c9b15
RatSpaceSteam <jacobalexanderpatt@gmail.com>
2023-10-19 14:54:56 -0500

    What? -Joe Biden

--- main.py
+++ main.py
@@ -6,29 +6,32 @@ class PhotoEditTestApp(App):
     pass
 
 class Display(Screen):
-    def display_image(self):
-        return image1
-
-    #def advance_image(self):
-        #global index
-        #if index < len(images)-1:
-            #index += 1
-        #else:
-            #index = 0
-        #self.ids.image.source = self.display_image()
-
-    #def back_image(self):
-        #global index
-        #if index > 0:
-            #index -= 1
-        #else:
-            #index = len(images)-1
-       # self.ids.image.source = self.display_image()
-
+    def verify_image(self, text):
+        if self.ids.name.text not in images:
+            return image0
+        else:
+            if self.ids.name.text == images[0]:
+                return images[0]
+            elif self.ids.name.text == images[1]:
+                return images[1]
+            elif self.ids.name.text == images[2]:
+                return images[2]
+            elif self.ids.name.text == images[3]:
+                return images[3]
+    def display_image(self, button):
+        if self.verify_image(self.ids.name.text) == image0:
+            return image0
+        elif self.verify_image(self.ids.name.text) == "dualvector.jpeg":
+            return images[0]
+        elif self.verify_image(self.ids.name.text) == "hypersphere.jpeg":
+            return images[1]
+        elif self.verify_image(self.ids.name.text) == "pyramid.jpeg":
+            return images[2]
+        elif self.verify_image(self.ids.name.text) == "venus.jpeg":
+            return images[3]
 
 images = ["dualvector.jpeg", "hypersphere.jpeg", "pyramid.jpeg", "venus.jpeg"]
-image1 = "dualvector.jpeg"
-index = 0
+image0 = "placeholder.png"
 
 
 PhotoEditTestApp().run()
