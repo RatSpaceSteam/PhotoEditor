5ba11014f053b96c66f015387a6396a6dad1ec90
RatSpaceSteam <jacobalexanderpatt@gmail.com>
2023-10-19 11:15:50 -0500

    Removed advance buttons

--- main.py
+++ main.py
@@ -7,26 +7,27 @@ class PhotoEditTestApp(App):
 
 class Display(Screen):
     def display_image(self):
-        return images[index]
+        return image1
 
-    def advance_image(self):
-        global index
-        if index < len(images)-1:
-            index += 1
-        else:
-            index = 0
-        self.ids.image.source = self.display_image()
+    #def advance_image(self):
+        #global index
+        #if index < len(images)-1:
+            #index += 1
+        #else:
+            #index = 0
+        #self.ids.image.source = self.display_image()
 
-    def back_image(self):
-        global index
-        if index > 0:
-            index -= 1
-        else:
-            index = len(images)-1
-        self.ids.image.source = self.display_image()
+    #def back_image(self):
+        #global index
+        #if index > 0:
+            #index -= 1
+        #else:
+            #index = len(images)-1
+       # self.ids.image.source = self.display_image()
 
 
 images = ["dualvector.jpeg", "hypersphere.jpeg", "pyramid.jpeg", "venus.jpeg"]
+image1 = "dualvector.jpeg"
 index = 0
 
 
