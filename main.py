3f48fa5fd105b713293c8c1ca6d4be7ecaca80a0
RatSpaceSteam <jacobalexanderpatt@gmail.com>
2023-10-19 11:03:24 -0500

    Added proper buttons

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
 from kivy.app import App
 from kivy.uix.screenmanager import Screen
 
-class PhotoEditApp(App):
+
+class PhotoEditTestApp(App):
     pass
+
+class Display(Screen):
+    def display_image(self):
+        return images[index]
+
+    def advance_image(self):
+        global index
+        if index < len(images)-1:
+            index += 1
+        else:
+            index = 0
+        self.ids.image.source = self.display_image()
+
+    def back_image(self):
+        global index
+        if index > 0:
+            index -= 1
+        else:
+            index = len(images)-1
+        self.ids.image.source = self.display_image()
+
+
+images = ["dualvector.jpeg", "hypersphere.jpeg", "pyramid.jpeg", "venus.jpeg"]
+index = 0
+
+
+PhotoEditTestApp().run()
